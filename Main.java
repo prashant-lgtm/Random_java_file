321b8d6a4b13024d90624b547c8a2422ee9a5617
PRASHANT RAJPUT <pr5784279@gmail.com>
2026-03-17 22:49:04 +0530

    Initial Code Added

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main{
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        int a=20;
+        int b=5;
+        System.out.println(a+b);
+        String name = "ram";
+    }
+}
